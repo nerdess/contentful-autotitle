aadd5761cfa46a87bdb99dd9fd5926aad729b197
Sissi <sylvia.adamski@gmail.com>
2023-07-07 18:06:24 +0200

    almonst finished: adding reference fields

--- src/locations/Field.jsx
+++ src/locations/Field.jsx
@@ -1,52 +1,118 @@
+import { useEffect, useState, useMemo } from 'react';
 import { Note, Stack, TextLink } from '@contentful/f36-components';
 import { useSDK } from '@contentful/react-apps-toolkit';
-import { useEffect, useState } from 'react';
 import { TextInput, FormControl } from '@contentful/f36-components';
+
 
 const DATE_FORMAT = { year: 'numeric', month: '2-digit', day: '2-digit' };
 
+const getEntryTitle =  (sdk, id ) => {
+
+	return new Promise((resolve, reject) => {
+
+		sdk?.space.getEntry(id)
+		.then((entry) => {
+	
+			sdk.space
+				.getContentType(entry.sys.contentType.sys.id)
+				.then((contentType) => {
+					resolve(entry.fields[contentType.displayField]);
+				});
+	
+		})
+		.catch(error => {
+		  console.log('Error:', error);
+		  reject(error);
+		});
+
+	});
+
+}
+
 const getLang = () => {
-	if (navigator.languages != undefined) return navigator.languages[0];
+	if (navigator.languages !== undefined) return navigator.languages[0];
 	return navigator.language;
 };
+
+const getValue = (field) => {
+
+	if (field.type === 'Date' && field.getValue()) {
+		const date = new Date(field.getValue());
+		return date.toLocaleDateString(getLang(), DATE_FORMAT);
+		
+	}
+	return field.getValue();
+}
 
 const Field = () => {
 
 	const sdk = useSDK();
+	const [entryValues, setEntryValues] = useState({});
+
+	const locale = sdk.locales.default;
 	const window = sdk.window;
-	const fieldIds = sdk.parameters.instance.fieldIds.split(',').map((fieldId) => fieldId.trim());
-	const fieldsMetadata = sdk.contentType.fields.filter(({ id }) => fieldIds.includes(id));
-	const [value, setValue] = useState(sdk.field.getValue());
-	const notFound = [];
-	const fields = fieldIds
-		.map((fieldId) => {
+
+	const fieldIds = useMemo(() => sdk.parameters.instance.fieldIds.split(',').map((fieldId) => fieldId.trim()), [sdk]);
+	const fieldsMetadata = fieldIds.map((fieldId) => sdk.contentType.fields.find(({ id }) => id === fieldId)).filter((field) => field);
+
+
+	const fields = useMemo(() => {
+		return fieldIds.map((fieldId) => {
 			const field = sdk.entry.fields[fieldId];
-			if (!field) notFound.push(fieldId);
 			return field;
 		})
 		.filter((field) => field);
+	}, [sdk, fieldIds]) 
 
-	fields.forEach((field) => {
-		field.onValueChanged(() => {
-			const values = fields
-				.map((field) => {
-					if (field.type === 'Date' && field.getValue()) {
-						const date = new Date(field.getValue());
-						return date.toLocaleDateString(getLang(), DATE_FORMAT);
-					}
+	const notFound = fieldIds.filter((fieldId) => !sdk.entry.fields[fieldId])
 
-					return field.getValue();
-				})
-				.filter((value) => value);
 
-			const joined = values.join(' - ');
+	useEffect(() => {
 
-			if (joined !== value) {
-				sdk.field.setValue(joined);
-				setValue(joined);
+		fields.forEach((field) => {
+
+			//grab all static fields (the referenced ones)
+			if (field.type === 'Link' && field.getValue()) {
+				const {
+					sys
+				} = field.getValue();
+				
+				getEntryTitle(sdk, sys.id).then((title) => {
+					setEntryValues((prev) => ({
+						...prev,
+						[field.id]: title[locale]
+					}));
+				});
 			}
+
+			//then set listeners to the fields that can change
+			field.onValueChanged(() => {
+
+				if (field.type === 'Link') return;
+
+				setEntryValues((prev) => ({
+					...prev,
+					[field.id]: getValue(field)
+				}));
+
+			});
 		});
-	});
+
+		return () => setEntryValues({});
+		
+	}, [fields, sdk, setEntryValues, locale])
+
+
+	const result = fieldIds.map((fieldId) => entryValues[fieldId] || null);
+	const resultFiltered = result.filter((value) => value);
+	const joined = resultFiltered.join(' - ');
+
+	console.log('joined', joined);
+
+	if (joined !== sdk.field.getValue() && fieldIds.length - notFound.length === result.length) {
+		sdk.field.setValue(joined);
+	}
+	
 
 	useEffect(() => {
 		window.startAutoResizer();
@@ -84,7 +150,7 @@ const Field = () => {
 		<Stack flexDirection='column' spacing='spacingS'>
 			<div style={{ width: '100%' }}>
 				<TextInput
-					value={value}
+					value={sdk.field.getValue()}
 					type='text'
 					name='autotitle'
 					isDisabled={true}
